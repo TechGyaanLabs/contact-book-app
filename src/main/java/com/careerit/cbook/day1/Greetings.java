c3c8d25d50189a5124fafbba0344d4ab638c01b6
TechGyaanLabs <techgyaanlabs@gmail.com>
2025-02-06 08:11:49 +0530

    Update Greetings.java
    Added extra messages

--- src/main/java/com/careerit/cbook/day1/Greetings.java
+++ src/main/java/com/careerit/cbook/day1/Greetings.java
@@ -6,5 +6,11 @@ public class Greetings {
         System.out.println("Welcome to Python Programming");
         System.out.println("Welcome to Java Programming");
         System.out.println("Welcome to .Net Programming");
+
+
+        System.out.println("Learning Programming is fun!");
+
+
+        System.out.println("Learn programming with fun");
     }
 }
