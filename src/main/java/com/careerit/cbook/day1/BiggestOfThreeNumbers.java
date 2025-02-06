75902cd04a2bc07f499aa774adf9efe08383443d
TechGyaanLabs <techgyaanlabs@gmail.com>
2025-02-06 07:50:55 +0530

    CA-1009 fixed issue, changed to ternary operator.

--- src/main/java/com/careerit/cbook/day1/BiggestOfThreeNumbers.java
+++ src/main/java/com/careerit/cbook/day1/BiggestOfThreeNumbers.java
@@ -5,9 +5,9 @@ public class BiggestOfThreeNumbers {
     public static void main(String[] args) {
 
         int num1 = 50;
-        int num2 = 20;
+        int num2 = 90;
         int num3 = 30;
-        int big = Math.max(num1, Math.max(num2, num3));
+        int big = (num1 > num2 && num1 > num3) ? num1 : Math.max(num2, num3);
         System.out.println("The biggest of "+num1+", "+num2+" and "+num3+" is "+big);
 
     }
