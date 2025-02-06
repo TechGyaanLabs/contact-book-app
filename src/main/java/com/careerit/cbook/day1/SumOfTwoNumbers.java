0ae00ea6cc90838c2d3d3a4e9f6907afe0587bd3
TechGyaanLabs <techgyaanlabs@gmail.com>
2025-02-06 07:44:20 +0530

    Added biggest of three numbers

--- src/main/java/com/careerit/cbook/day1/SumOfTwoNumbers.java
+++ src/main/java/com/careerit/cbook/day1/SumOfTwoNumbers.java
@@ -6,6 +6,8 @@ public class SumOfTwoNumbers {
         int num1 = 10;
         int num2 = 20;
         int sum = num1 + num2;
+        int sub = num1 - num2;
         System.out.println("The sum of "+num1+" and "+num2+" is "+sum);
+        System.out.println("The sub of "+num1+" and "+num2+" is "+sub);
     }
 }
